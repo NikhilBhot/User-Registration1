f8ef4825693934c6ffe6a95b34e31439aaeb924d
unknown <bhotnikhil25@gmail.com>
2023-01-06 00:47:19 +0530

    [Nikhil]-Validate First Name Of User

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UserRegistration1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter The First Name : ");
+            string FName = Console.ReadLine();
+            UserRegistartion registration = new UserRegistartion();
+            registration.ValidFirstName(FName);
+            registration.ValidateFName(FName);
+        }
+    }
+}
---
+++ UserRegistartion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration1
+{
+    public class UserRegistartion
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z][A-za-z]{2,}$";
+        public string ValidFirstName(string input) => Regex.IsMatch(input, FIRST_NAME_REGEX) ? "Valid Name" : "Invalid Name";
+        public void ValidateFName(string input)
+        {
+
+            bool res=Regex.IsMatch(input, FIRST_NAME_REGEX);
+            if (res==true)
+            {
+
+                Console.WriteLine("UserName Is Valid");
+            }
+            else
+            {
+                Console.WriteLine("User Name Is Invalid");
+            }
+
+           
+        }
+
+    }
+}
