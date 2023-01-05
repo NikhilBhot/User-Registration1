a2841fe61e7ad8929a046b76b9a7d5d6ee59ff9a
unknown <bhotnikhil25@gmail.com>
2023-01-06 01:30:09 +0530

    [Nikhil]-Validate User Mobile No

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@
             Console.Write("Enter the Email : ");
             string Email = Console.ReadLine();
 
+            Console.Write("Enter the Mobile Number : ");
+            string MobileNumber = Console.ReadLine();
 
             UserRegistartion registration = new UserRegistartion();
             registration.ValidFirstName(FName);
@@ -25,6 +27,10 @@
             UserRegistartion registration2 = new UserRegistartion();
             registration.ValidEmail(Email);
             registration.ValidateEmail(Email);
+
+            UserRegistartion registration3 = new UserRegistartion();
+            registration3.ValidMobileNumber(MobileNumber);
+            registration.ValidateMobileNo(MobileNumber);
         }
     }
 }
--- UserRegistartion.cs
+++ UserRegistartion.cs
@@ -12,6 +12,7 @@ namespace UserRegistration1
         public const string FIRST_NAME_REGEX = "^[A-Z][A-za-z]{2,}$";
         public const string LAST_NAME_REGEX = "^[A-Z][A-za-z]{2,}$";
         public const string EMAIL = "^[A-Za-z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}$";
+        public const string MOBILE_NUMBER = "^(91)[' ']?[7-9]{1}[0-9]{9}$";
         public string ValidFirstName(string input) => Regex.IsMatch(input, FIRST_NAME_REGEX) ? "true" : "flase";
         public void ValidateFName(string input)
         {
@@ -56,5 +57,19 @@ namespace UserRegistration1
 
         }
 
+        public string ValidMobileNumber(string input) => Regex.IsMatch(input, MOBILE_NUMBER) ? "true" : "flase";
+        public void ValidateMobileNo(string input)
+        {
+            bool res = Regex.IsMatch(input, EMAIL);
+            if (res == true)
+            {
+                Console.WriteLine("User Mobile No Is Valid");
+            }
+            else
+            {
+                Console.WriteLine("User Mobile No Is Invalid");
+            }
+
+        }
     }
 }
